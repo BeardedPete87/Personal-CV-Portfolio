b18fd1cf7111e65b1451a6758a588acfef4f3916
Peter Fitzsimmons <couple12101@hotmail.co.uk>
2025-02-27 09:31:13 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // toggle icon navbar
 let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('navbar');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
@@ -30,4 +30,8 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active'); 
 }
